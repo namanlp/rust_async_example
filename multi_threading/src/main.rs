0fd63cdd39f868fc276240d9b4f8b58de296b9c2
namanlp <naman@garudalinux.org>
2023-11-24 02:27:47 +0530

    Update multithreading example

--- multi_threading/src/main.rs
+++ multi_threading/src/main.rs
@@ -1,21 +1,36 @@
+use std::collections::VecDeque;
 use std::thread;
+use std::time::Duration;
 
 
-fn print_hello(){
-    for i in 0..10 {
-        println!("HELLO {}", i);
-    }
+fn print_hello(num : usize){
+    println!("Executing thread number : {}", num);
+
+    // Thread gets blocked after 10 second,
+    // hence the execution passes to other threads.
+
+    // Current thread is sent to waiting queue.
+    // Read Operating Systems for more information.
+    thread::sleep(Duration::from_millis(10_000));
 }
 
 fn main() {
 
-    let t1 = thread::spawn(print_hello);
-    let t2 = thread::spawn(print_hello);
-    let t3 = thread::spawn(print_hello);
-    let t4 = thread::spawn(print_hello);
+    let mut thread_queue = VecDeque::new();
 
-    t1.join().unwrap();
-    t2.join().unwrap();
-    t3.join().unwrap();
-    t4.join().unwrap();
+    for i in 0..100 {
+
+        // Push Thread to Queue
+        thread_queue.push_back(
+            thread::spawn(move || {
+                print_hello(i);
+            })
+        )
+    }
+
+    // Join for each thread in the queue
+    while thread_queue.len() > 0 {
+        println!("Pulled a thread");
+        thread_queue.pop_front().unwrap().join().unwrap();
+    }
 }
